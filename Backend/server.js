4ea9f3be2dacee36bdbec107596d2df02b208ee0
mayankkarn2 <mayankkarn2@msitprogram.net>
2021-02-25 01:31:43 +0530

    Configuring Server

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,39 @@
 // modules =================================================
 const express = require('express');
 const app = express();
+var mongoose = require('mongoose');
+
 // set our port
 const port = 3000;
+
+// configuration ===========================================
+
+var db = require('./config/db');
+console.log("connecting--",db);
+mongoose.connect(db.url);
+
+// frontend routes =========================================================
 app.get('/', (req, res) => res.send('Welcome to Tutorialspoint!'));
+
+//defining route
+app.get('/tproute', function (req, res) {
+    res.send('This is routing for the application developed using Node and Express...');
+});
+
+// sample api route
+
+var Student = require('./models/Task');
+app.get('/api/task', function(req, res) {
+    // use mongoose to get all students in the database
+    Student.find(function(err, students) {
+       // if there is an error retrieving, send the error.
+       // nothing after res.send(err) will execute
+       if (err)
+          res.send(err);
+       res.json(students); // return all students in JSON format
+    });
+ });
+ 
 
 // startup our app at http://localhost:3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
